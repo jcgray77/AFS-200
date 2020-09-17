e0eb624b4607542cd097cee4d21357ff9c8d3be1
Jessica Christine Gray <60930017+jcgray77@users.noreply.github.com>
2020-09-17 16:41:05 -0400

    Week1

---
+++ week1/Input/input.py
@@ -0,0 +1,5 @@
+city=input("tell me your city: ")
+print("your city is " + city)
+
+zipcode=input("tell me your zipcode: ")
+print("your zipcode is " + zipcode)
---
+++ week1/Print/message.py
@@ -0,0 +1,8 @@
+msg = "I love python"
+print(msg)
+
+msg = "I can not wait to graduate"
+print(msg)
+
+msg = "This is so exciting"
+print(msg)
