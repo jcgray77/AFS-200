4a037018c5bcb448a4cdee987e75cead18ae2a3b
Jessica Christine Gray <60930017+jcgray77@users.noreply.github.com>
2020-10-04 17:07:19 -0400

    week 2

---
+++ week2/class/class.py
@@ -0,0 +1,9 @@
+class myClass():
+  def method1(self):
+      print("Guru99")
+        
+  def method2(self,someString):    
+      print("Software Testing:" + someString)
+
+user_input = input("What's your favorite language? ")
+print("My favorite language is ", user_input.upper())
---
+++ week2/variable/variable.py
@@ -0,0 +1,6 @@
+f ='Your son John is failing 2 subjects'
+print(f)
+f ='John will need to redo 2 courses'
+print(f)
+f ='Eric is doing well in geography'
+print(f)
