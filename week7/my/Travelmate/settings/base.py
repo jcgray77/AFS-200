343855746671a424ed6c9a1c258a1807c173e844
Jessica Christine Gray <60930017+jcgray77@users.noreply.github.com>
2020-11-10 15:44:10 -0500

    week7

---
+++ week7/my/Travelmate/settings/base.py
@@ -0,0 +1,120 @@
+from django.core.urlresolvers import reverse_lazy
+from os.path import dirname, join, exists
+
+
+BASE_DIR = dirname(dirname(dirname(__file__)))
+STATICFILES_DIRS = [join(BASE_DIR, 'static')]
+MEDIA_ROOT = join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            join(BASE_DIR, 'templates'),
+           
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+import environ
+env = environ.Env()
+
+
+env_file = join(dirname(__file__), 'local.env')
+if exists(env_file):
+    environ.Env.read_env(str(env_file))
+
+
+SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = []
+
+
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django_admin_bootstrapped',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'authtools',
+    'crispy_forms',
+    'easy_thumbnails',
+
+    'profiles',
+    'accounts',
+    'website',
+
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'TravelMate.urls'
+
+WSGI_APPLICATION = 'TravelMate.wsgi.application'
+
+
+
+DATABASES = {
+  
+    'default': env.db(),
+}
+
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+
+STATIC_URL = '/static/'
+
+ALLOWED_HOSTS = []
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+
+from django.contrib import messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+AUTH_USER_MODEL = 'authtools.User'
+LOGIN_REDIRECT_URL = reverse_lazy("profiles:show_self")
+LOGIN_URL = reverse_lazy("accounts:login")
+
+THUMBNAIL_EXTENSION = 'png'     
---
+++ week7/my/Travelmate/settings/development.py
@@ -0,0 +1,72 @@
+from .base import *             
+import sys
+import logging.config
+
+
+DEBUG = True
+TEMPLATES[0]['OPTIONS'].update({'debug': True})
+
+
+if "celery" in sys.argv[0]:
+    DEBUG = False
+
+
+INSTALLED_APPS += (
+    'debug_toolbar.apps.DebugToolbarConfig',)
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+THUMBNAIL_DEBUG = True
+
+
+LOGFILE_ROOT = join(dirname(BASE_DIR), 'logs')
+
+
+
+LOGGING_CONFIG = None
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(pathname)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'django_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': join(LOGFILE_ROOT, 'django.log'),
+            'formatter': 'verbose'
+        },
+        'proj_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': join(LOGFILE_ROOT, 'project.log'),
+            'formatter': 'verbose'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['django_log_file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'project': {
+            'handlers': ['proj_log_file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+logging.config.dictConfig(LOGGING)
